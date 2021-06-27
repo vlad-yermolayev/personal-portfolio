d6e8e9d1e3e6359f568944523b56f1d4632eb282
Vlad <ermac1988@mail.ru>
2021-06-27 16:03:05 +0300

    Added DOMContentLoaded

---
+++ js/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const HEADER = document.querySelector('.header');
+  const BUTTON = HEADER.querySelector('.header__button');
+  const YEAR_ELEMENT = document.querySelector('.footer__year');
+  const CURRENT_YEAR = new Date().getFullYear();
+
+  YEAR_ELEMENT.textContent = CURRENT_YEAR;
+
+  BUTTON.addEventListener('click', (e) => {
+    HEADER.classList.toggle('header--menu-open');
+    console.log('Add');
+  });
+});
